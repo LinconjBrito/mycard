00f54e9acdc8257e970dd42a4678ef10468dda5c
LinconjBrito <linconjbrito@gmail.com>
2023-08-03 19:29:37 -0300

    Aula finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,13 @@ class MeuApp extends StatelessWidget {
         body: SafeArea(
           child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const CircleAvatar(
                   radius: 40,
                   backgroundImage: AssetImage('images/profile.jpg'),
                 ),
-               const Text(
+                const Text(
                   "Lincon Brito",
                   style: TextStyle(
                       fontFamily: 'Pacifico',
@@ -33,7 +34,45 @@ class MeuApp extends StatelessWidget {
                   style: TextStyle(
                       fontFamily: 'Source Sans',
                       fontWeight: FontWeight.bold,
-                      fontSize: 20, letterSpacing: 2.5, color: Colors.deepOrange.shade100),
+                      fontSize: 20,
+                      letterSpacing: 2.5,
+                      color: Colors.deepOrange.shade100),
+                ),
+
+                SizedBox(
+                  width: 250.0,
+                  height: 20,
+                  child: Divider(color: Colors.deepOrange.shade100),
+                ),
+                const Card(
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: ListTile(
+                    leading: Icon(color: Colors.deepOrange, Icons.phone),
+                    title: Text(
+                      "71 99139-5530",
+                      style: TextStyle(
+                          color: Colors.deepOrange,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Source Sans',
+                          letterSpacing: 2),
+                    ),
+                  ),
+                ),
+
+                
+                const Card(
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20.0),
+                  child: ListTile(
+                    leading: Icon(color: Colors.deepOrange, Icons.email),
+                    title: Text(
+                      "linconbrito15@gmail.com",
+                      style: TextStyle(
+                          color: Colors.deepOrange,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Source Sans',
+                          letterSpacing: 1),
+                    ),
+                  ),
                 )
               ],
             ),
